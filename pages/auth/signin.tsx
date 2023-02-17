4c5a01cd9de4710247688b72767b77d26d4342c6
alam-gir <info.alamgirhussain@gmail.com>
2023-02-17 15:50:35 +0600

    login btn designed small

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -3,11 +3,17 @@ import React from "react";
 
 const signin = () => {
   return (
-    <div>
-      <button onClick={() => signIn("github", { callbackUrl: "/" })}>
+    <div className="flex flex-col h-screen justify-center items-center gap-3">
+      <button
+        onClick={() => signIn("github", { callbackUrl: "/" })}
+        className="bg-[#e50914] text-white py-2 px-4 text-lg"
+      >
         sign in with {"GITHUB"}
       </button>
-      <button onClick={() => signIn("google", { callbackUrl: "/" })}>
+      <button
+        onClick={() => signIn("google", { callbackUrl: "/" })}
+        className="bg-[#e50914] text-white py-2 px-4 text-lg"
+      >
         sign in with {"GOOGLE"}
       </button>
     </div>
